a07ec1c97e073da58a47f91560c3802c8061a09a
Li <lc934@cornell.edu>
2019-02-05 07:07:59 -0500

    bar

--- scripts/page-progress.js
+++ scripts/page-progress.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-	var pageProgressFunction = setInterval(function(){
-		var documentHeight = $(document).height();
-		var scrollFromTop = $(document).scrollTop() + $(window).height();
-		var pageProgress = Math.round(scrollFromTop / documentHeight * 100);
-		var pageProgressPercent = pageProgress + '%';
-		// console.log(pageProgress);
-		if($(document).scrollTop() != 0) {
-			$('.page-progress-item').animate({
-				width: pageProgressPercent
-			}, 200);
-		} else {
-			$('.page-progress-item').css('width', 0);
-		}
-	}, 250);
-});
+// $(document).ready(function(){
+// 	var pageProgressFunction = setInterval(function(){
+// 		var documentHeight = $(document).height();
+// 		var scrollFromTop = $(document).scrollTop() + $(window).height();
+// 		var pageProgress = Math.round(scrollFromTop / documentHeight * 100);
+// 		var pageProgressPercent = pageProgress + '%';
+// 		// console.log(pageProgress);
+// 		if($(document).scrollTop() != 0) {
+// 			$('.page-progress-item').animate({
+// 				width: pageProgressPercent
+// 			}, 200);
+// 		} else {
+// 			$('.page-progress-item').css('width', 0);
+// 		}
+// 	}, 250);
+// });
+window.onscroll = function() {pageProgressFunction()};
+
+function pageProgressFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
