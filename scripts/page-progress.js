f19c1c1165302883343c4593f5a53d7fa8a1cd74
Li <lc934@cornell.edu>
2019-02-06 10:45:08 -0500

    p

--- scripts/page-progress.js
+++ scripts/page-progress.js
@@ -22,3 +22,32 @@ function pageProgressFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("progressBar").style.width = scrolled + "%";
 }
+
+// window.onscroll = function() {
+//   // Shrink/Expand navbar height
+//   if ($(document).scrollTop() > $("#content_container").offset()["top"] - 80) {
+//       $(".navbar").css({"padding-top": "20px", "padding-bottom": "20px"});
+//       $(".navbar_container").css({"box-shadow": "0 0 10px rgba(0, 0, 0, 0.2)", "background-color": "white"});
+
+//       // Making sidenav fixed
+//       $(".sidenav").css({
+//           "position": "fixed",
+//           "top": "80px"
+//       });
+//   } else {
+//       $(".navbar").css({"padding-top": "30px", "padding-bottom": "30px"});
+//       $(".navbar_container").css({"box-shadow": "0 0 10px rgba(0, 0, 0, 0)", "background-color": "rgba(250, 250, 250, 0)"});
+
+//       // Making sidenav fixed
+//       $(".sidenav").css({
+//           "position": "inherit",
+//           "top": "auto"
+//       });
+//   }
+
+//   // Update the width of the progress bar
+//   var thanksTop = $(".thanks").offset().top - (window.innerHeight * 0.8) - $("#content_container").offset().top;
+//   var progressBarTop = $("#progress_bar").offset().top - $("#content_container").offset().top;
+//   var percentage = (progressBarTop / thanksTop) * 100;
+//   $("#progress_bar").css("width", percentage + "vw");
+// }
